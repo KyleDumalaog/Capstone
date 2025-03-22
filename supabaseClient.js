c0c7ab83bc1bfafada9eeeedb8e305cc0b300fed
KyleDumalaog <kylerogerdumalaog@gmail.com>
2025-03-22 16:42:04 +0800

    test

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
-//import { createClient } from "https://esm.sh/@supabase/supabase-js";
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
 const SUPABASE_URL = "https://jixmkwoddokkwbztayta.supabase.co";  
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2MjU4MTQsImV4cCI6MjA1ODIwMTgxNH0.sUZmp9u-Q8l1yP62GJgmQazcvX7attJ9E9lkekxf9zc";  // 🔹 Replace this!
 
