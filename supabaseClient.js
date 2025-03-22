036c9e0c327a5f3b24663990a59d1957197b10f2
KyleDumalaog <kylerogerdumalaog@gmail.com>
2025-03-22 14:31:29 +0800

    test 1.5.

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabaseUrl = 'https://jixmkwoddokkwbztayta.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjU3ODM5NiwiZXhwIjoyMDU4MTU0Mzk2fQ.y9185WLXpkVcb7mOZvke-79wYROn-tnbGKGS41lyh2o'; // ⚠️ Use Service Role Key
