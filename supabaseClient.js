a756e123b66ecb5875e34739e5a8a84899638b5c
KyleDumalaog <kylerogerdumalaog@gmail.com>
2025-03-22 14:26:51 +0800

    test 1.4.2.2

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://jixmkwoddokkwbztayta.supabase.co";  
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzgzOTYsImV4cCI6MjA1ODE1NDM5Nn0.4TrgLwww0iTKA-ix9jEB9sp4_GO3bcV3vdhxmLPQlRM";  
-
+const supabaseUrl = 'https://jixmkwoddokkwbztayta.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjU3ODM5NiwiZXhwIjoyMDU4MTU0Mzk2fQ.y9185WLXpkVcb7mOZvke-79wYROn-tnbGKGS41lyh2o'; // ⚠️ Use Service Role Key
 export const supabase = createClient(supabaseUrl, supabaseKey);
