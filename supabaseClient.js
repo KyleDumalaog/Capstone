d14dd41eb3a70dc1aecba4402ae1d82981138603
KyleDumalaog <kylerogerdumalaog@gmail.com>
2025-03-26 00:58:56 +0800

    debug

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const SUPABASE_URL = "https://jixmkwoddokkwbztayta.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MTY1NDUsImV4cCI6MjA1ODQ5MjU0NX0.owRlb1xnhYfm6jMLQMZ8bV9dsXL_ILckFKX2tM49FMw"; // ✅ Use your actual ANON key
