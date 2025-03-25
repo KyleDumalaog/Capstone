75f560ed5320d3edf2dadeeaa2582d48d70939dc
KyleDumalaog <kylerogerdumalaog@gmail.com>
2025-03-26 01:10:35 +0800

    debugplspls

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const SUPABASE_URL = "https://jixmkwoddokkwbztayta.supabase.co";  
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2MjU4MTQsImV4cCI6MjA1ODIwMTgxNH0.sUZmp9u-Q8l1yP62GJgmQazcvX7attJ9E9lkekxf9zc";  // 🔹 Replace this!
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppeG1rd29kZG9ra3dienRheXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MTY1NDUsImV4cCI6MjA1ODQ5MjU0NX0.owRlb1xnhYfm6jMLQMZ8bV9dsXL_ILckFKX2tM49FMw";  // 🔹 Replace this!
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY); /*{
     fetch: (url, options) => {
